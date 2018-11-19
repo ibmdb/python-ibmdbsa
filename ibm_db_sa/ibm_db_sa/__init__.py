d9a808a646babe07ddc7776771ecb5de1dab6d3b
ValkA <valentin.dashinsky@il.ibm.com>
2018-11-20 00:04:40 +0200

    version number update

--- ibm_db_sa/ibm_db_sa/__init__.py
+++ ibm_db_sa/ibm_db_sa/__init__.py
@@ -17,7 +17,7 @@
 # | Contributors: Jaimy Azle, Mike Bayer                                     |
 # +--------------------------------------------------------------------------+
 
-__version__ = '0.3.3'
+__version__ = '1.3.3'
 
 from . import ibm_db, pyodbc, base, zxjdbc
 
