d63335b59b98d2ae0125a54f83e3a4bccdd15dfd
bchoudhary6415 <95407446+bchoudhary6415@users.noreply.github.com>
2023-04-20 14:36:09 +0530

    Changes for support of SQLAlchemy 2.0.x (#127)
    Signed-off-by: bchoudhary <bchoudhary@rocketsoftware.com>

--- ibm_db_sa/ibm_db_as400.py
+++ ibm_db_sa/ibm_db_as400.py
@@ -1,6 +1,11 @@
-from .base import DB2ExecutionContext, DB2Dialect
-from sqlalchemy import processors, types as sa_types, util
 from sqlalchemy import __version__ as SA_Version
+SA_Version = [int(ver_token) for ver_token in SA_Version.split('.')[0:2]]
+from .base import DB2ExecutionContext, DB2Dialect
+if SA_Version < [2,0]:
+    from sqlalchemy import processors, types as sa_types, util
+else:
+    from sqlalchemy import types as sa_types, util
+    from sqlalchemy.engine import processors
 from sqlalchemy.exc import ArgumentError
 from ibm_db_sa.ibm_db import DB2Dialect_ibm_db
 from ibm_db_sa.reflection import AS400Reflector
