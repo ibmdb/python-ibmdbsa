9133865a1e7d607f3ffada0811b27f9e5f51da11
Michael Maltese <michaeljosephmaltese@gmail.com>
2025-10-29 02:38:59 -0400

    use correct AS400 reflector when dbms_name/_ver is none (#183)
    Fixes #182
    
    Signed-off-by: Michael Maltese <michaeljosephmaltese@gmail.com>

--- ibm_db_sa/base.py
+++ ibm_db_sa/base.py
@@ -790,13 +790,13 @@ class DB2Dialect(default.DefaultDialect):
         DB2Dialect.serverType = self.dbms_name
         super(DB2Dialect, self).initialize(connection)
         # check server type logic here
-        _reflector_cls = ibm_reflection.DB2Reflector
+        _reflector_cls = self._reflector_cls
         if self.dbms_name == 'AS':
             _reflector_cls = ibm_reflection.AS400Reflector
         elif self.dbms_name == "DB2":
             _reflector_cls = ibm_reflection.OS390Reflector
         elif(self.dbms_name is None):
-            _reflector_cls = ibm_reflection.DB2Reflector
+            pass
         elif "DB2/" in self.dbms_name:
             _reflector_cls = ibm_reflection.DB2Reflector
         elif "IDS/" in self.dbms_name:
